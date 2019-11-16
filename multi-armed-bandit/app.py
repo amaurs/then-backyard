73b0f913f2c76955cf7cea6c561223774ef71c93
Amaury Gutierrez <acoamaur@amazon.com>
2019-11-16 08:46:03 -0800

    😸 First test and stub of service.

--- multi-armed-bandit/app.py
+++ multi-armed-bandit/app.py
@@ -1,4 +1,7 @@
-from chalice import Chalice
+import logging
+import random
+
+from chalice import Chalice, Response
 
 app = Chalice(app_name='multi-armed-bandit')
 
@@ -6,6 +9,23 @@ app = Chalice(app_name='multi-armed-bandit')
 @app.route('/')
 def index():
     return {'hello': 'world'}
+
+@app.route('/next', methods=['POST'])
+def index():
+    request = app.current_request
+    body = request.json_body
+
+
+    logging.info(body.get("available"))
+    logging.info(body.get("visited"))
+    logging.info(body.get("state"))
+    logging.info(body.get("reward"))
+
+    new_state = random.choice(list(set(body.get("available")) - set(body.get("visited"))))
+
+
+    return Response(body={'state': new_state},
+                    status_code=200)
 
 
 # The view function above will return {"hello": "world"}

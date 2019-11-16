9f7af44ccfb3c23b372487fd85b9d65f58a0b270
amaurs <amaury.gtz@gmail.com>
2019-11-16 13:09:34 -0800

    ⛓ Adding cors support.

--- multi-armed-bandit/app.py
+++ multi-armed-bandit/app.py
@@ -6,7 +6,7 @@ from chalice import Chalice, Response
 app = Chalice(app_name='multi-armed-bandit')
 
 
-@app.route('/next', methods=['POST'])
+@app.route('/next', methods=['POST'], cors=True)
 def index():
     request = app.current_request
     body = request.json_body
@@ -24,24 +24,3 @@ def index():
 
     return Response(body={'state': new_state},
                     status_code=200)
-
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#

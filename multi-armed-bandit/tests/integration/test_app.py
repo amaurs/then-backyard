04874acfbafcb966ebcd518bd0a4041397c7f43c
amaurs <amaury.gtz@gmail.com>
2019-11-16 16:26:31 -0800

    🧽 Added a test for all visited.

--- multi-armed-bandit/tests/integration/test_app.py
+++ multi-armed-bandit/tests/integration/test_app.py
@@ -30,6 +30,25 @@ class TestApp(unittest.TestCase):
         assert response['statusCode'] == 200
         assert body.get('state') in ["PAGE_2", "PAGE_4"]
 
+    def test_all_visited(self):
+        body = {"available": ["PAGE_1", "PAGE_2", "PAGE_3", "PAGE_4", "PAGE_5"], 
+                "visited": ["PAGE_1", "PAGE_2", "PAGE_3", "PAGE_4", "PAGE_5"],
+                "state": "PAGE_3",
+                "reward": 700}
+        response = self.gateway.handle_request(method='POST',
+                                               path='/next',
+                                               headers={
+                                                  'Content-Type': 'application/json'
+                                               },
+                                               body=json.dumps(body))
+
+        logging.info(response)
+
+        body = json.loads(response['body'])
+
+        assert response['statusCode'] == 200
+        assert body.get('state') is None
+
 
 if __name__ == '__main__':
     unittest.main()

b3e2006507b85f2b71797b98801bc4480e777ac5
amaurs <amaury.gtz@gmail.com>
2019-11-17 23:02:56 -0800

    🤓 Logging configuration.

--- multi-armed-bandit/app.py
+++ multi-armed-bandit/app.py
@@ -4,14 +4,18 @@ import random
 from chalice import Chalice, Response
 
 app = Chalice(app_name='multi-armed-bandit')
-
+app.log.setLevel(logging.INFO)
 
 @app.route('/order', methods=['POST'], cors=True)
 def order():
     request = app.current_request
     body = request.json_body
+    
     states = body.get("states")
-    return Response(body={'order': random.sample(states, len(states))},
+    app.log.info("States available: %s" % states)
+    new_order = random.sample(states, len(states))
+    app.log.info("New order: %s" % new_order)
+    return Response(body={'order': new_order},
                     status_code=200)
 
 @app.route('/metric', methods=['POST'], cors=True)

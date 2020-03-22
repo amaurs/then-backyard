98dfa8fc4ec14846dad4135165f221ca5714ce63
amaurs <amaury.gtz@gmail.com>
2020-03-22 12:42:04 -0700

    👨🏽‍💼 Adding traveling salesman feature to server to centrilize assets.

--- multi-armed-bandit/app.py
+++ multi-armed-bandit/app.py
@@ -2,8 +2,13 @@ import json
 import logging
 import random
 import os
+import uuid
+import subprocess
+import csv
+import math
 
 import boto3
+import subprocess
 import urllib.parse
 from chalice import Chalice, Response
 import markovify
@@ -12,6 +17,10 @@ app = Chalice(app_name='multi-armed-bandit')
 app.log.setLevel(logging.INFO)
 
 EPSILON = 0.15
+
+FROM = 0
+TO = 1
+FACTOR = 1000
 
 es_file = os.path.join(os.path.dirname(__file__), 'chalicelib', 'boleros_es.txt')
 en_file = os.path.join(os.path.dirname(__file__), 'chalicelib', 'boleros_en.txt')
@@ -153,3 +162,195 @@ def sentence_en():
     return json.dumps(d, ensure_ascii=False)
 
 
+
+def create_instance_3d(filename, n_cities, point_set):
+    cities = []
+    with open(filename, "w+") as file_handle:
+        file_handle.write("%d\n" % n_cities)
+        for i in range(n_cities):
+            if point_set == "moebius":
+                city = generate_random_moebius()
+            elif point_set == "sphere":
+                city = generate_random_sphere()
+            elif point_set == "torus":
+                city = generate_random_torus()
+            elif point_set == "tree_foil":
+                city = generate_random_trefoil()
+            elif point_set == "helix":
+                city = generate_random_helix()
+            elif point_set == "plane":
+                city = generate_random_plane()
+            file_handle.write("%s %s %s\n" % (city[0] * FACTOR, city[1] * FACTOR, city[2] * FACTOR))
+            cities.append(city)
+        file_handle.write("EOF\n")
+    return cities
+
+
+
+
+def create_instance_from_cities_3d(filename, cities):
+    final_cities = []
+    with open(filename, "w+") as file_handle:
+        print(len(cities) / 3)
+        file_handle.write("%d\n" % (len(cities) / 3))
+        for i in range(0, len(cities), 3):
+            city = [cities[i], cities[i + 1], cities[i + 2]]
+            print(city)
+            file_handle.write("%s %s %s\n" % (city[0], city[1], city[2]))
+            final_cities.append(city)
+        file_handle.write("EOF\n")
+    return final_cities
+
+def create_instance_from_cities_2d(filename, cities):
+    final_cities = []
+    with open(filename, "w+") as file_handle:
+        print(len(cities) / 2)
+        file_handle.write("%d\n" % (len(cities) / 2))
+        for i in range(0, len(cities), 2):
+            city = [cities[i], cities[i + 1]]
+            print(city)
+            file_handle.write("%s %s\n" % (city[0], city[1]))
+            final_cities.append(city)
+        file_handle.write("EOF\n")
+    return final_cities
+
+
+def generate_random_moebius():
+    s = random.random() * 2 - 1
+    theta = random.random() * math.pi * 2
+    return from_moebius_coords(2.0, s, theta)
+
+def from_moebius_coords(radius, s, theta):
+    x = (radius + s * math.cos(theta / 2)) * math.cos(theta)
+    y = (radius + s * math.cos(theta / 2)) * math.sin(theta)
+    z = s * math.sin(theta / 2)
+    return [x, y, z]
+
+def generate_random_sphere():
+    radius = 1.0
+    theta = random.random() * math.pi * 2
+    phi = random.random() * math.pi
+    return from_sphere_coords(radius, theta, phi)
+
+def from_sphere_coords(rho, theta, phi):
+    x = rho * math.cos(theta) * math.sin(phi)
+    y = rho * math.sin(theta) * math.sin(phi)
+    z = rho * math.cos(phi)
+    return [x, y, z]
+
+def generate_random_torus():
+    a = 2.0
+    c = 1.0
+    v = random.random() * math.pi * 2
+    u = random.random() * math.pi * 2
+    return from_torus_coords(a, c, u, v)
+
+def from_torus_coords(a, c, u, v):
+    x = (c * a * math.cos(v)) * math.cos(u)
+    y = (c * a * math.cos(v)) * math.sin(u)
+    z = a * math.sin(u)
+    return [x, y, z]
+
+def generate_random_trefoil():
+    u = random.random() * math.pi * 2
+    return from_trefoil_coords(u)
+
+def from_trefoil_coords(u):
+    x = math.sin(u) + 2.0 * math.sin(2.0 * u)
+    y = math.cos(u) - 2.0 * math.cos(2.0 * u)
+    z = - math.sin(3.0 * u)
+    return [x, y, z]
+
+def generate_random_helix():
+    u = random.random() * math.pi * 2
+    v = 2.0
+    return from_helix_coords(u, v)
+
+def from_helix_coords(u, v):
+    x = v * math.cos(u)
+    y = v * math.sin(u)
+    z = u
+    return [x, y, z]
+
+def generate_random_plane():
+    x = random.random() * 2 - 1
+    y = random.random() * 2 - 1
+    z = 0
+    return [x, y, z]
+
+def execute(cmd):
+    app.log.info(" ".join(cmd))
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
+def call_solver(tsp_file, tour_file, dim=3):
+    cmd = ["./chalicelib/linkern", "-o", tour_file, "-N", "%s" % dim, tsp_file]
+    for line in execute(cmd):
+        app.log.info(line)
+
+def implement_tour(tour_file, cities):
+    final_tour = []
+    with open(tour_file, 'r') as file_handle:
+        csv_reader = csv.reader(file_handle, delimiter=' ')
+        next(csv_reader) # skip the first row which do not contain any city
+        first = None
+        for edge in csv_reader:
+            app.log.info("edge %s" % edge)
+            if first is None:
+                first = cities[int(edge[FROM])]
+            final_tour += cities[int(edge[FROM])]
+        final_tour += first
+    return final_tour
+
+@app.route('/', methods=['POST'], cors=True)
+def index():
+    body = app.current_request.json_body
+    app.log.info("headers: %s", app.current_request.headers)
+    app.log.info("context: %s", app.current_request.context)
+    app.log.info("json_body: %s", app.current_request.json_body)
+    app.log.info("method: %s", app.current_request.method)
+    app.log.info("query_params: %s", app.current_request.query_params)
+    app.log.info("raw_body: %s", app.current_request.raw_body)
+    app.log.info("stage_vars: %s", app.current_request.stage_vars)
+    app.log.info("to_dict: %s", app.current_request.to_dict)
+    app.log.info("uri_params: %s", app.current_request.uri_params)
+    point_set = body['point_set']
+    n_cities = min(int(body['n_cities']), 3000)
+
+    tsp_file = "/tmp/%s.tsp" % uuid.uuid4()
+    tour_file = "/tmp/%s.tour" % uuid.uuid4()
+
+    cities = create_instance_3d(tsp_file, n_cities, point_set)
+    call_solver(tsp_file, tour_file)
+    tour = implement_tour(tour_file, cities)
+
+
+    return tour
+
+@app.route('/solve', methods=['GET'], cors=True)
+def solver():
+    cities = json.loads(app.current_request.query_params.get('cities'))
+    dim = int(app.current_request.query_params.get('dimension', 2))
+    app.log.info(dim)
+    tsp_file = "/tmp/%s.tsp" % uuid.uuid4()
+    tour_file = "/tmp/%s.tour" % uuid.uuid4()
+
+    if dim == 3:
+
+        final_cities = create_instance_from_cities_3d(tsp_file, cities)
+        call_solver(tsp_file, tour_file, dim=3)
+    elif dim == 2:
+        final_cities = create_instance_from_cities_2d(tsp_file, cities)
+        call_solver(tsp_file, tour_file, dim=2)
+
+    else:
+        return {"status": 500}
+
+    tour = implement_tour(tour_file, final_cities)
+
+    return tour
